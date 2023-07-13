4b71b8aa1da48e082b8855fbbe59936ffa8c1968
Abdullah Zafar <mabdullahzafar08@gmail.com>
2023-07-13 11:53:39 +0500

    changed index, detail and result to generic views

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.http import Http404, HttpResponseRedirect
 from django.http import HttpResponse
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from django.utils import timezone
 
@@ -10,20 +11,22 @@ from django.shortcuts import get_object_or_404, render
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    data = {"question_list": latest_question_list,}
-    return HttpResponse(render(request, 'polls/index.html', data))
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def choice(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
