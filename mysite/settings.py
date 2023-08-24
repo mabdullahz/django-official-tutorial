5167411ff0d97617abc0271449064d395602ccb9
Abdullah Zafar <mabdullahzafar08@gmail.com>
2023-08-24 09:50:31 +0500

    media images

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,6 +135,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = '/images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published", auto_now_add=True)
+    image = models.ImageField(upload_to='questions/', default=None, null=True)
 
     def __str__(self):
         return self.question_text
